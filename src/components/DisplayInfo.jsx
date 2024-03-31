e8cdd9061cf2fc3bf00aa20924b46ef67a00552b
Edvo1901 <DongVo1901@gmail.com>
2024-03-31 17:11:17 +1030

    practice useeffect

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,61 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./DisplayInfo.scss";
 import logo from "./../logo.svg";
-
-// class DisplayInfo extends React.Component {
-//     state = {
-//         showHide: true,
-//     }
-
-//     showHideList = (event) => {
-//         this.setState({
-//             showHide: !this.state.showHide
-//         });
-//     }
-
-//     componentDidMount() {
-//         setTimeout(() => {
-//             document.title = "Maybe website"
-//         }, 3000)
-//     }
-
-//     componentDidUpdate(prevProps, prevState) {
-//         console.log("Did update ", this.props, prevProps)
-
-//     }
-
-//     render() {
-//         const { name, age, listUsers, removeUser } = this.props;
-//         const { showHide } = this.state;
-//         return (
-//             <div className="display-info-container">
-//                 <img src={logo} alt="logoImg"/>
-//                 <div>
-//                     <button onClick={(event) => this.showHideList(event)}>
-//                         {showHide === true ? "Hide list user" : "Show list user"}
-//                     </button>
-//                 </div>
-//                 {
-//                     showHide &&
-//                     <div>
-//                         {
-//                             listUsers.map((user) => {
-//                                 return (
-//                                     <label key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                         User {user.id}, Name: {user.name}, Age: {user.age}
-//                                         <button onClick={() => removeUser(user.id)}>X</button>
-//                                         <br />
-//                                     </label>
-//                                 )
-//                             })
-//                         }
-//                     </div>
-//                 }
-
-//             </div>
-//         )
-//     }
-// }
 
 const DisplayInfo = (props) => {
     const {listUsers, removeUser} = props;
@@ -64,6 +9,13 @@ const DisplayInfo = (props) => {
     const showHideList = () => {
         setShowHide(!showHide);
     }
+
+    useEffect(() => {
+        if (listUsers.length === 0) {
+            alert("Deleted all")
+        }
+        console.log("Call me useffect")
+    }, [listUsers])
 
     return (
         <div className="display-info-container">
