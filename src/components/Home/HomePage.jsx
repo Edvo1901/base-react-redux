8c7e2cc48f888b30fd0f9426f7810b138b69a835
Edvo1901 <DongVo1901@gmail.com>
2024-04-02 02:17:52 +1030

    adding homepage video

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,11 @@
+import videoHomePage from '../../assets/video-homepage.mp4';
+
 const HomePage = () => {
     return (
         <div className="homepage-container">
-            This is home page
+            <video autoPlay muted loop>
+                <source src={videoHomePage} type="video/mp4"/>
+            </video>
         </div>
     )
 }
