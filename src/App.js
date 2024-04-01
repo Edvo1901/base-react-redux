4cade0c10b59efea6cade156c8e77f5aef760217
Edvo1901 <DongVo1901@gmail.com>
2024-04-02 00:42:14 +1030

    update header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.scss';
 import Header from './components/Header/Header.jsx';
+import {Link} from 'react-router-dom';
 
 const App = () => {
 	return (
 		<div className="app-container">
 			<Header/>
+			<div>
+				<button>
+					<Link to="/user">User page</Link>
+				</button>
+				<button>
+					<Link to="/admin">Admin page</Link>
+				</button>
+			</div>
 		</div>
 	);
 }
