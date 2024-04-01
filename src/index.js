2455e9f542a014d3707a6123bcd778c09bd7087b
Edvo1901 <DongVo1901@gmail.com>
2024-04-02 01:13:07 +1030

    index route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import Admin from './components/Admin/Admin.jsx';
-import User from './components/User/User.jsx';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App';
+import Admin from './components/Admin/Admin.jsx';
+import User from './components/User/User.jsx';
+import HomePage from './components/Home/HomePage.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +18,7 @@ root.render(
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<App />}>
+						<Route index element={<HomePage />} />
 						<Route path="/admin" element={<Admin />} />
 						<Route path="/user" element={<User />} />
 					</Route>
