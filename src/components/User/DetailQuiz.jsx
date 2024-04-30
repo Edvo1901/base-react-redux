0b5dbe5e34f0398fcf85c1bc020d5f7c98f72c30
Edvo1901 <DongVo1901@gmail.com>
2024-04-30 20:04:18 +0930

    display question

--- src/components/User/DetailQuiz.jsx
+++ src/components/User/DetailQuiz.jsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../services/APIService";
 import _ from "lodash";
+import "./DetailQuiz.scss";
 
 const DetailQuiz = () => {
     const params = useParams()
+    const location = useLocation()
     const quizId = params.id
+
+    console.log(location)
 
     const fetchQuestions = async () => {
         const res = await getDataQuiz(quizId)
@@ -22,7 +26,7 @@ const DetailQuiz = () => {
                     }
                     answers.push(item.answers)
                 })
-                return {questionId: key, data: value, questionDescription, image}
+                return { questionId: key, data: value, questionDescription, image }
             }).value();
         }
     }
@@ -30,10 +34,40 @@ const DetailQuiz = () => {
     useEffect(() => {
         fetchQuestions()
     }, [quizId])
-    
+
     return (
         <div className="detail-quiz-container">
-            DetailQuiz
+            <div className="left-content">
+                <div className="title">
+                    Quiz {quizId}: {location?.state?.quizTitle}
+                </div>
+                <hr/>
+                <div className="q-Body">
+                    <img />
+                </div>
+                <div className="q-content">
+                    <div className="question">Question 1: How are you</div>
+                    <div className="answer">
+                        <div>
+                            A. Testing
+                        </div>
+                        <div>
+                            B. Testing
+                        </div>
+                        <div>
+                            C. Testing
+                        </div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button className="btn btn-secondary">Prev</button>
+                    <button className="btn btn-primary">Next</button>
+                </div>
+
+            </div>
+            <div className="right-content">
+                Countdown
+            </div>
         </div>
     )
 }
