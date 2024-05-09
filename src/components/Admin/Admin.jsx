783c936ce85da146ca7ad7c98c2c58f5dd73741f
Edvo1901 <DongVo1901@gmail.com>
2024-05-10 00:59:59 +0930

    admin dashboard design

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -2,11 +2,31 @@ import SideBar from "./SideBar";
 import './Admin.scss';
 import { FaBars } from 'react-icons/fa';
 import { useState } from "react";
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import PerfectScrollbar from 'react-perfect-scrollbar';
+import Language from "../Header/Language";
+import { NavDropdown } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../services/APIService";
+import { toast } from "react-toastify";
+import { doLogout } from "../../redux/action/userAction";
 
 const Admin = (props) => {
+    const account = useSelector(state => state.user.account)
     const [collapsed, setCollapsed] = useState(false);
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleLogout = async () => {
+        let res = await logout(account.email, account.refresh_token)
+        if (res && res.EC === 0) {
+            dispatch(doLogout())
+            navigate("/")
+            toast.success(res.EM)
+        } else {
+            toast.error(res.EM)
+        }
+    }
 
     return (
         <div className="admin-container">
@@ -15,7 +35,17 @@ const Admin = (props) => {
             </div>
             <div className="admin-content">
                 <div className="admin-header">
-                    <FaBars onClick={() => setCollapsed(!collapsed)} />
+                    <span onClick={() => setCollapsed(!collapsed)}>
+                        <FaBars className="left-side"/>
+                    </span>
+
+                    <div className="right-side">
+                        <Language />
+                        <NavDropdown title="Setting" id="basic-nav-dropdown">
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => handleLogout()}>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
                 </div>
                 <div className="admin-main">
                     <PerfectScrollbar>
